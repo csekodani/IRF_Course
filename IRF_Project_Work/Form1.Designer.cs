a3dfdc391f9baa3ac46f5c58913dccc7032fabcf
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-12-13 16:16:38 +0100

    everything has been put to working directory

---
+++ IRF_Project_Work/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace IRF_Project_Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.radioBtn_ID = new System.Windows.Forms.RadioButton();
+            this.radioBtn_Coord = new System.Windows.Forms.RadioButton();
+            this.radioBtn_Name = new System.Windows.Forms.RadioButton();
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.textBox_Coord = new System.Windows.Forms.TextBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.btn_GO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.MainLabel.Location = new System.Drawing.Point(36, 28);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(198, 25);
+            this.MainLabel.TabIndex = 0;
+            this.MainLabel.Text = "Keresett város adata:";
+            this.MainLabel.UseMnemonic = false;
+            // 
+            // radioBtn_ID
+            // 
+            this.radioBtn_ID.AutoSize = true;
+            this.radioBtn_ID.Location = new System.Drawing.Point(41, 80);
+            this.radioBtn_ID.Name = "radioBtn_ID";
+            this.radioBtn_ID.Size = new System.Drawing.Size(36, 17);
+            this.radioBtn_ID.TabIndex = 1;
+            this.radioBtn_ID.TabStop = true;
+            this.radioBtn_ID.Text = "ID";
+            this.radioBtn_ID.UseVisualStyleBackColor = true;
+            this.radioBtn_ID.CheckedChanged += new System.EventHandler(this.radioBtn_ID_CheckedChanged);
+            // 
+            // radioBtn_Coord
+            // 
+            this.radioBtn_Coord.AutoSize = true;
+            this.radioBtn_Coord.Location = new System.Drawing.Point(41, 113);
+            this.radioBtn_Coord.Name = "radioBtn_Coord";
+            this.radioBtn_Coord.Size = new System.Drawing.Size(79, 17);
+            this.radioBtn_Coord.TabIndex = 2;
+            this.radioBtn_Coord.TabStop = true;
+            this.radioBtn_Coord.Text = "Koordináta:";
+            this.radioBtn_Coord.UseVisualStyleBackColor = true;
+            this.radioBtn_Coord.CheckedChanged += new System.EventHandler(this.radioBtn_Coord_CheckedChanged);
+            // 
+            // radioBtn_Name
+            // 
+            this.radioBtn_Name.AutoSize = true;
+            this.radioBtn_Name.Location = new System.Drawing.Point(41, 147);
+            this.radioBtn_Name.Name = "radioBtn_Name";
+            this.radioBtn_Name.Size = new System.Drawing.Size(45, 17);
+            this.radioBtn_Name.TabIndex = 3;
+            this.radioBtn_Name.TabStop = true;
+            this.radioBtn_Name.Text = "Név";
+            this.radioBtn_Name.UseVisualStyleBackColor = true;
+            this.radioBtn_Name.CheckedChanged += new System.EventHandler(this.radioBtn_Name_CheckedChanged);
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Location = new System.Drawing.Point(134, 80);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(100, 20);
+            this.textBox_ID.TabIndex = 4;
+            // 
+            // textBox_Coord
+            // 
+            this.textBox_Coord.Location = new System.Drawing.Point(134, 112);
+            this.textBox_Coord.Name = "textBox_Coord";
+            this.textBox_Coord.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Coord.TabIndex = 5;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(134, 147);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Name.TabIndex = 6;
+            // 
+            // btn_GO
+            // 
+            this.btn_GO.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_GO.Location = new System.Drawing.Point(694, 390);
+            this.btn_GO.Name = "btn_GO";
+            this.btn_GO.Size = new System.Drawing.Size(75, 48);
+            this.btn_GO.TabIndex = 7;
+            this.btn_GO.Text = "Go";
+            this.btn_GO.UseVisualStyleBackColor = true;
+            this.btn_GO.Click += new System.EventHandler(this.btn_GO_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_GO);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.textBox_Coord);
+            this.Controls.Add(this.textBox_ID);
+            this.Controls.Add(this.radioBtn_Name);
+            this.Controls.Add(this.radioBtn_Coord);
+            this.Controls.Add(this.radioBtn_ID);
+            this.Controls.Add(this.MainLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.RadioButton radioBtn_ID;
+        private System.Windows.Forms.RadioButton radioBtn_Coord;
+        private System.Windows.Forms.RadioButton radioBtn_Name;
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.TextBox textBox_Coord;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.Button btn_GO;
+    }
+}
+
---
+++ IRF_Project_Work/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_Project_Work
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_GO_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioBtn_ID_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox_ID.Visible = true;
+            textBox_Coord.Visible = false;
+            textBox_Name.Visible = false;
+
+        }
+
+        private void radioBtn_Coord_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox_ID.Visible = false;
+            textBox_Coord.Visible = true;
+            textBox_Name.Visible = false;
+
+        }
+
+        private void radioBtn_Name_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox_ID.Visible = false;
+            textBox_Coord.Visible = false;
+            textBox_Name.Visible = true;
+        }
+    }
+}
