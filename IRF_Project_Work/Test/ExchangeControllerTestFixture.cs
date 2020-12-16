d4c9c5d52988c5f239cdd9aabec2f339366c7eba
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-12-16 02:38:38 +0100

    Test implemented

---
+++ IRF_Project_Work/Test/ExchangeControllerTestFixture.cs
@@ -0,0 +1,48 @@
+﻿using IRF_Project_Work.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project_Work.Test
+{
+    public class ExchangeControllerTestFixture
+    {
+        [Test,
+            TestCase(-1.0,false),
+            TestCase(25,true),
+            TestCase(1001,false)
+
+        ]
+        public void TestValidateValue(decimal value, bool expectedResult)
+        {
+            //arrange
+            var exchangeController = new ExchangeController();
+            //act
+            var actualResult = exchangeController.ValidateValue(value);
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test,
+            TestCase("USD",true),
+            TestCase("AYAY",false),
+            TestCase("",false)
+        ]
+        public void TestValidateFavorite(string value, bool expectedResult)
+        {
+            //arrange
+            var exchangeController = new ExchangeController();
+            //act
+            var actualResult = exchangeController.ValidateFavorite(value);
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+    }
+}
